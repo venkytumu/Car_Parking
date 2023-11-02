f228fcace7022bb680a10e2a059671d60420eb82
VinayReddy555 <vinay5kumarreddynla@gmail.com>
2023-11-02 14:43:26 +0530

    removed braces

--- accounts/urls.py
+++ accounts/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("register",views.register,name="register"),
+    path("register",views.register,name="register",
     path("login",views.login,name="login"),
     path("logout",views.logout,name="logout"),
     path("resetlink",views.resetlink,name="resetlink"),
